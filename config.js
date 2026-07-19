1df0eb924951e82023790dc6f6485d86f78a8e96
trostals <todd.rostalski@frontier.com>
2026-07-19 08:05:39 -0700

    2026-07-19 Driver Updates

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const PLAYER_PICKS = {
-  Blake:      { id: 3859, name: "Blake" }, // logano
-  The_DAD:    { id: 4180, name: "The DAD" }, // Cindric
-  Lisa_Marie: { id: 3873, name: "Lisa Marie" }, //A Dillion
-  Lisa_Lane:  { id: 1816, name: "Lisa Lane" }, // keselowski
-  Todd:       { id: 4045, name: "Todd" } // Bowman
+  Blake:      { id: 4045, name: "Blake" }, // Blowman
+  The_DAD:    { id: 4123, name: "The DAD" }, // Berry
+  Lisa_Marie: { id: 3859, name: "Lisa Marie" }, //Oregano
+  Lisa_Lane:  { id: 4180, name: "Lisa Lane" }, // Cindric
+  Todd:       { id: 4001, name: "Todd" } // Chastain
 };
